f597996694bf39e851913dfa18bfcb56719f9c72
xim85 <joaquin.albors01@gmail.com>
2025-06-17 23:35:05 +0200

    commit

--- ejercicio04/index.js
+++ ejercicio04/index.js
@@ -1,5 +1,11 @@
 //1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el evento click que ejecute un console log con la información del evento del click
 
+const button = document.getElementById('btnToClick')
+
+button.addEventListener('click', (event) => {
+  console.log('Evento de clic:', event)
+})
+
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
