e856107b2f02306f302fc07dfe9bb4462bb60694
xim85 <joaquin.albors01@gmail.com>
2025-06-17 23:52:10 +0200

    Update index.js

--- ejercicio04/index.js
+++ ejercicio04/index.js
@@ -8,4 +8,16 @@ button.addEventListener('click', (event) => {
 
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
+const focusInput = document.querySelector('.focus')
+
+focusInput.addEventListener('focus', () => {
+  console.log('Valor del input al enfocarse:', focusInput.value)
+})
+
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+
+const valueInput = document.querySelector('.value')
+
+valueInput.addEventListener('input', () => {
+  console.log('Valor actual del input:', valueInput.value)
+})
